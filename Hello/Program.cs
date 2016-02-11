76421b4f8414f1d7389b01ab31eee26a4f61d9f9
ggokulakrishnan <gokul.g@hotmail.com>
2016-02-12 01:09:19 +0530

    Initial base code for version control (GIT)

---
+++ Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sri Krishnarpanam !!!");
+            Console.WriteLine("Project Created to test CheckIn CheckOut in GIT Source control repository");
+            Console.ReadLine();
+        }
+    }
+}
